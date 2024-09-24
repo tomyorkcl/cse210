8ab811dd3d6f3c15af12006c06eb031151d4d4f4
tomyorkcl <tomas.contrerast98@gmail.com>
2024-09-23 21:29:46 -0300

    work done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string Value = Console.ReadLine();
+
+        int number = int.Parse(Value);
+        string grade = "";
+
+
+        if (number > 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else if (number < 60)
+        {
+            grade = "F";
+        }
+
+
+        Console.WriteLine("Here your result: ");
+
+        if (number >= 70)
+        {
+            Console.WriteLine($"Your grade is {grade}. You aprove the course!");
+        }
+        else
+        {
+            Console.WriteLine("You don't passed, try again next time!");
+        }
     }
 }
