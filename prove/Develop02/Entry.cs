6424aa253cdf53254fdce858a98deb12f99a1d00
tomyorkcl <tomas.contrerast98@gmail.com>
2024-09-26 12:48:47 -0300

    new assignments done

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,11 @@
+public class Entry
+{
+    public string _date;
+    public string _promtText;
+    public string _entryText;
+    
+    public void Display()
+    {
+        
+    }
+}
---
+++ prove/Develop02/PromtGenerator.cs
@@ -0,0 +1,9 @@
+public class PromptGenerator
+{
+    public List<string> _prompts;
+
+    public string GetRandomPromt()
+    {
+        return "";
+    }
+}
