4e09ce941c93b105d3d66020215f79213e7d238c
tomyorkcl <tomas.contrerast98@gmail.com>
2024-10-27 02:41:26 -0300

    Activities update

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Cyan", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Purple", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
